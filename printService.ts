4a4a47e5db9e20ed154a792f5eb9c7cbdaf587d8
Trev S <134303044+mhiproducts@users.noreply.github.com>
2023-12-11 20:45:38 -0600

    Updated printService.ts

--- printService.ts
+++ printService.ts
@@ -8,11 +8,11 @@ interface PrintJob {
     status: 'pending' | 'printing' | 'completed' | 'failed';
   }
   
-  // Class to manage print jobs
+// Class to manage print jobs
   class PrintService {
     private printJobs: PrintJob[] = [];
   
-    // Method to add a new print job
+// Method to add a new print job
     addPrintJob(userId: string, fileName: string, filePath: string): void {
       const newPrintJob: PrintJob = {
         userId,
@@ -25,24 +25,24 @@ interface PrintJob {
       this.processPrintJob(newPrintJob);
     }
   
-    // Private method to simulate processing and printing a job
+// Private method to simulate processing and printing a job
     private processPrintJob(printJob: PrintJob): void {
-      // Simulate processing and printing
+// Simulate processing and printing
       printJob.status = 'printing';
   
-      // Simulate printing completion after 5 seconds
+// Simulate printing completion after 5 seconds
       setTimeout(() => {
         printJob.status = 'completed';
         this.notifyUser(printJob);
       }, 5000);
     }
   
-    // Private method to notify the user when a print job is completed
+// Private method to notify the user when a print job is completed
     private notifyUser(printJob: PrintJob): void {
       console.log(`Print job for ${printJob.fileName} completed. Ready for pickup.`);
     }
   
-    // Method to retrieve the list of print jobs
+// Method to retrieve the list of print jobs
     getPrintJobs(): PrintJob[] {
       return this.printJobs;
     }
